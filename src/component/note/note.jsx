16706f571f5b0b240a0b3571ec547212326dbc0e
kalanig23 <kalanivmrgg@gmail.com>
2024-04-30 20:59:31 +0530

    notes message initial render render

--- src/component/note/note.jsx
+++ src/component/note/note.jsx
@@ -1,28 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../rightPart/rightPart.module.css';
 
 const Note = ({selectedItemid}) => {
+    const noteListData = JSON.parse(localStorage.getItem('noteList'));
+    const [noteStateListData, setNoteStateListData] = useState();
+
     useEffect(() => {
-        const noteListData = localStorage.getItem('noteList');
-      },);
+        const filterNoteStateListData = noteListData.filter((data)=>data.groupId == selectedItemid)
+        setNoteStateListData(filterNoteStateListData);
+    },[]);
+
 
     return(
         <div className={styles.noteMessageContainer}>
-                    <div className={styles.noteMessage}>
-                        Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their days writing projects, words are already flowing from their fingers.
-                        <div className={styles.messageDateTime}>
-                            <span>9 Mar 2023</span>
-                            <span>10:10 AM</span>
-                        </div>
-                    </div>
-                    <div className={styles.noteMessage}>
-                        Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their days writing projects, words are already flowing from their fingers.
-                        <div className={styles.messageDateTime}>
-                            <span>9 Mar 2023</span>
-                            <span>10:10 AM</span>
-                        </div>
+            {noteStateListData?.map((noteData, index)=>(
+                <div className={styles.noteMessage} key={index}>
+                    {noteData.message}
+                    <div className={styles.messageDateTime}>
+                        <span>{noteData.date}</span>
+                        <span>{noteData.time}</span>
                     </div>
                 </div>
+            ))}
+        </div>
     )
 }
 
