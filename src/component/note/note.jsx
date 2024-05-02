54deb76ebfb4f6570d1f295eae279f60cde2be4f
kalanig23 <kalanivmrgg@gmail.com>
2024-05-02 23:02:01 +0530

    fixed the notemessage using context

--- src/component/note/note.jsx
+++ src/component/note/note.jsx
@@ -1,18 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import styles from '../rightPart/rightPart.module.css';
+import {NoteListContext} from '../../App';
 
 const Note = ({selectedItemid}) => {
-    const noteListData = JSON.parse(localStorage.getItem('noteList'));
-    const [noteStateListData, setNoteStateListData] = useState();
-
-    useEffect(() => {
-        const filterNoteStateListData = noteListData.filter((data)=>data.groupId == selectedItemid)
-        setNoteStateListData(filterNoteStateListData);
-    },[]);
+    const {noteLists, setNoteLists} = useContext(NoteListContext);
 
     return(
         <div className={styles.noteMessageContainer}>
-            {noteStateListData?.map((noteData, index)=>(
+            {noteLists.filter((d)=>d.groupId==selectedItemid)?.map((noteData, index)=>(
                 <div className={styles.noteMessage} key={index}>
                     {noteData.message}
                     <div className={styles.messageDateTime}>
--- src/component/rightPart/rightPart.jsx
+++ src/component/rightPart/rightPart.jsx
@@ -1,15 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import RightFrontNote from '../../assets/frontnote.svg';
 import styles from './rightPart.module.css';
 import arraowLeft from '../../assets/arrowsubmit.svg';
 import Note from '../note/note';
 import lock from '../../assets/lock.svg';
+import {NoteListContext} from '../../App';
 
 const RightPart = ({ groupListValueProps, selectedItemid}) => {
+    const {noteLists, setNoteLists} = useContext(NoteListContext);
+    console.log('data', noteLists);
     const [groupListValue, setGroupListValue] = useState([]);
     const [ids, setIds] = useState();
     const [noteMessage, setNoteMessage] = useState('');
-    const [noteList, setNoteList] = useState([]);
+    // const [noteList, setNoteList] = useState([]);
+
+    useEffect(()=>{
+        const value = JSON.parse(localStorage.getItem('noteList'));
+        setNoteLists([...value]);
+    },[])
 
     useEffect(()=>{
         if(selectedItemid === ids) {
@@ -24,7 +32,7 @@ const RightPart = ({ groupListValueProps, selectedItemid}) => {
     const submitNoteMessage = () => {
         if (noteMessage.length) {
             let object = {message: noteMessage, date: dateFormate(), time:timeFormat(), groupId: ids}
-            setNoteList((noteList)=>[...noteList, object]);
+            setNoteLists((noteLists)=>[...noteLists, object]);
             setNoteMessage('')
         } else {
             alert('message is empty!!');
@@ -32,14 +40,14 @@ const RightPart = ({ groupListValueProps, selectedItemid}) => {
     }
 
     useEffect(() => {
-        if (noteList.length !== 0 && noteList !== undefined) {
+        if (noteLists.length !== 0 && noteLists !== undefined) {
             try {
-                localStorage.setItem('noteList', JSON.stringify(noteList));
+                localStorage.setItem('noteLists', JSON.stringify(noteLists));
             } catch (error) {
-                console.error('Error storing noteList in localStorage:', error);
+                console.error('Error storing noteLists in localStorage:', error);
             }
         }
-    }, [noteList]);
+    }, [noteLists]);
 
     function dateFormate () {
         var currentDate = new Date();
